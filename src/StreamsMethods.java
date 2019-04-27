a71ad0826330ec798c3ce33f31e4e558b55942c7
holawlo <holawlo@gmail.com>
2019-04-27 23:11:59 +0200

    finished

--- src/StreamsMethods.java
+++ src/StreamsMethods.java
@@ -1,24 +1,35 @@
 import model.Gender;
 import model.Person;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamsMethods {
 
     public Person findOldestWoman(List<Person> people) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return people.stream()
+                .filter(p -> p.getGender().equals(Gender.FEMALE))
+                .max(Comparator.comparingInt(Person::getAge))
+                .orElse(null);
+
     }
 
     public List<String> getFullNamesUppercase(List<Person> people) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return people.stream()
+                .map(p -> p.getFirstName().toUpperCase() + " " + p.getLastName().toUpperCase())
+                .collect(Collectors.toList());
+
     }
 
     public Double getAverageAge(List<Person> people) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return people.stream()
+                .collect(Collectors.averagingDouble(Person::getAge));
     }
 
     public Map<Gender, Double> getAverageAgeByGender(List<Person> people) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return people.stream()
+                .collect(Collectors.groupingBy(p -> p.getGender(), Collectors.averagingDouble(Person::getAge)));
     }
 }
